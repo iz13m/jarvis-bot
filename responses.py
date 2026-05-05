519c999ff3288745fc5ce88ecff0d375e181b0c9
Islam-Abdelsalam-Turing <abdelsalam.i@turing.com>
2026-05-05 23:23:24 +0300

    Add date feature

--- responses.py
+++ responses.py
@@ -7,9 +7,11 @@ def get_response(message):
     elif "good evening" in message:
         return "Good evening! Time to relax and recharge."
 
-     elif "good afternoon" in message:
+    elif "good afternoon" in message:
         return "good after noon."
   
+    elif "What is today?":
+        return "Today is a great day!"
   
     else:
         return "Sorry, I don't understand that yet."
